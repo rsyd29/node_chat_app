eb7922dd0593b117c4779c20a2c3fbaae3cda39c
Budiman Rasyid <brasyid15@gmail.com>
2021-12-15 11:41:22 +0700

    Creating server and socket

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Working');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => {
+    console.log('Server is Started on port', PORT);
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('Connected Successfully', socket.id);
+});
+
