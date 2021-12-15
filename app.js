8c1078414477c93a5f52e7ec660ea4f516cd5bef
Budiman Rasyid <brasyid15@gmail.com>
2021-12-15 14:16:43 +0700

    Membuat berapa user yang connect ke dalam server

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const server = app.listen(PORT, () => {
 
 const io = require('socket.io')(server);
 
+const connectedUser = new Set();
+
 io.on('connection', (socket) => {
     console.log("Connected Successfully", socket.id);
+    connectedUser.add(socket.id);
+    io.emit('connected-user', connectedUser.size);
     socket.on('disconnect', () => {
         console.log("Disconnected", socket.id);
+        connectedUser.delete(socket.id);
+        io.emit('connected-user', connectedUser.size);
     });
 
     socket.on('message', (data) => {
